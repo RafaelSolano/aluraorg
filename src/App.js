65ae0a041e50bcb7f8a18f568b5f00499852c77f
Rafael Solano <rafaelsolanol007@gmail.com>
2023-04-09 23:13:24 -0500

    Se crea el header y el componente form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Header  from './components/Header/Header';
+import FormUser from './components/FormUser/FormUser';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          hello world!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        Aprende React
-        </a>
-      </header>
+      <Header/>
+      <FormUser/>
+    
+      
     </div>
   );
 }
---
+++ src/components/FormUser/FormUser.js
@@ -0,0 +1,12 @@
+import "./FormUser.css"
+
+const FormUser = () =>{
+    return <section className="formUser">
+        <form className="formUser__form">
+            <h2 className="formUser__title">Rellena el formulario para agregar el colaborador</h2>
+            
+        </form>
+    </section>
+}
+
+export default FormUser;
