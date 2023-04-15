77a892cb3a0f0ae38f8e5c708c7f0d9cdada38c5
Rafael Solano <rafaelsolanol007@gmail.com>
2023-04-15 13:41:59 -0500

    Se crea la concenion del los inputs del formulario y el btn

--- src/components/FieldInput/index.js
+++ src/components/FieldInput/index.js
@@ -1,9 +1,24 @@
+
+import { useState } from "react";
 import "./FieldForm.css"
 const FieldForm = (props) =>{
+
+    const [valor,actualizarValor] = useState("");
     const placeholderTemplate = `${props.placeholder}...`
+
+    const manejarCambios = (e) =>{
+        props.actualizarValor(e.target.value)
+    }
     return <div className="fieldForm"> 
-    <label className="fieldForm__label"  >{props.title} </label>
-    <input className="fieldForm__input" type="text"  name="fieldText" placeholder={placeholderTemplate} required={props.required}></input>
+    <label  className="fieldForm__label"  >{props.title} </label>
+    <input  className="fieldForm__input" 
+            type="text"  
+            name="fieldText" 
+            placeholder={placeholderTemplate} 
+            required={props.required}
+            value={props.valor}
+            onChange={manejarCambios}            
+            ></input>
 
 
     </div>
--- src/components/FormUser/FormUser.js
+++ src/components/FormUser/FormUser.js
@@ -1,20 +1,31 @@
+import { useState } from "react";
 import "./FormUser.css"
 import FieldForm from "../FieldInput";
 import ListOptions from "../ListOptions";
 import Button from "../Button";
 const FormUser = () =>{
+    const [personName , actualizarName] = useState("")
+    const [personPuesto , actualizarPuesto] = useState("")
+    const [personFoto , actualizarFoto] = useState("")
 
     const controlSubmit =(e) =>{
         e.preventDefault()
-        console.log('Envio btn', e) 
+        console.log("Manejar envio");
+        let datosEnviar = {
+            personName,
+            personPuesto ,
+            personFoto,
+            
+        }
+        console.log(datosEnviar);
     }
 
     return <section className="formUser">
         <form onSubmit={controlSubmit}  className="formUser__form">
             <h2 className="formUser__title">Rellena el formulario para agregar el colaborador</h2>
-            <FieldForm title ='Nombre: ' placeholder="Ingresar Nombre" required />
-            <FieldForm title ='Puesto: ' placeholder="Ingresar Puesto" required/>
-            <FieldForm title ='Foto:'placeholder="Ingresar enlace de Foto" required/>
+            <FieldForm title ='Nombre: ' placeholder="Ingresar Nombre" required valor= {personName} actualizarValor = {actualizarName}/>
+            <FieldForm title ='Puesto: ' placeholder="Ingresar Puesto" required valor ={personPuesto} actualizarValor = {actualizarPuesto} />
+            <FieldForm title ='Foto:'placeholder="Ingresar enlace de Foto" required valor ={personFoto} actualizarValor = {actualizarFoto} />
             <ListOptions />
             <Button>
                 Crear
